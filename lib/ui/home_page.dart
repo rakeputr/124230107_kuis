5ba4415c2ad4bdf93bd9161354913ab202802eef
rakeputr <rakeputri27@gmail.com>
2025-09-30 11:47:28 +0700

    feat : login page & homepage UI

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:template_quiz_mobile_si_b/models/game_model.dart';
+import 'package:template_quiz_mobile_si_b/ui/login_page.dart';
 
 class HomePage extends StatefulWidget {
   final String username;
@@ -11,6 +13,107 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFFEF3C3B),
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Welcome,',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+              Text(
+                widget.username,
+                style: TextStyle(fontSize: 14, color: Colors.white),
+              )
+            ],
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(context, MaterialPageRoute(
+                  builder: (context) {
+                    return LoginPage();
+                  },
+                ), (route) => false);
+              },
+              icon: Icon(Icons.logout_outlined),
+              color: Colors.white,
+            )
+          ],
+        ),
+        body: SingleChildScrollView(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: GridView.builder(
+                itemCount: GameModel.gameList.length,
+                shrinkWrap: true, // biar gabung sama ScrollView utama apk
+                physics: NeverScrollableScrollPhysics(), //offin scroll ganda
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 1, // ini biar tampil per 2 kolom
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                    childAspectRatio: 0.95),
+                itemBuilder: (context, index) {
+                  return _gameCard(context, index);
+                }),
+          )
+        ])));
+  }
+
+  Widget _gameCard(BuildContext context, int index) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Color.fromARGB(255, 253, 210, 209),
+          borderRadius: BorderRadius.circular(20)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+            child: SizedBox(
+              height: 250,
+              width: double.infinity,
+              child: Image.asset(GameModel.gameList[index].gameImg,
+                  fit: BoxFit.cover),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              children: [
+                Text(
+                  GameModel.gameList[index].gameName,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                    "${GameModel.gameList[index].gamePublisher} - ${GameModel.gameList[index].gamePublishDate}"),
+                Text(GameModel.gameList[index].gameDesc),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.favorite_outline),
+                    Text(GameModel.gameList[index].totalLike.toString()),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:template_quiz_mobile_si_b/ui/home_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,8 +9,112 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String username = "";
+  String password = "";
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Hello',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            'Welcome to the app!',
+            style: TextStyle(fontSize: 16, color: Colors.grey),
+          ),
+          _usernameField(),
+          _passwordField(),
+          _loginButton(context)
+        ],
+      ),
+    );
+  }
+
+  Widget _usernameField() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextFormField(
+        enabled: true,
+        onChanged: (value) {
+          username = value;
+        },
+        decoration: InputDecoration(
+          hintText: 'Username',
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0xFFEF3C3B)),
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0xFFEF3C3B)),
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _passwordField() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextFormField(
+        enabled: true,
+        onChanged: (value) {
+          password = value;
+        },
+        obscureText: true,
+        decoration: InputDecoration(
+          hintText: 'Password',
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0xFFEF3C3B)),
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0xFFEF3C3B)),
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _loginButton(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      width: MediaQuery.of(context).size.width,
+      child: ElevatedButton(
+        onPressed: () {
+          String message = "";
+          if (username == "rake" && password == "107") {
+            setState(() {
+              message = "Login berhasil!";
+            });
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) {
+              return HomePage(username: username);
+            }));
+          } else if (username == "" || password == "") {
+            setState(() {
+              message = "Username atau Password tidak boleh kosong!";
+            });
+          } else {
+            setState(() {
+              message = "Login gagal";
+            });
+          }
+
+          SnackBar messageBar = SnackBar(content: Text(message));
+          ScaffoldMessenger.of(context).showSnackBar(messageBar);
+        },
+        child: Text('Login'),
+        style: ElevatedButton.styleFrom(
+            minimumSize: Size(double.infinity, 55),
+            foregroundColor: Colors.white,
+            backgroundColor: Color(0xFFEF3C3B)),
+      ),
+    );
   }
 }
